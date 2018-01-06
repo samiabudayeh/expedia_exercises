20b8d6a2c31db09c11bd5ce8d078056ec7a830be
sami abudayeh <sami.abudeyah@hotmail.com>
2018-01-06 21:05:59 +0200

    expedia

--- src/main/java/com/hotmail/sami/config/WebInitializer.java
+++ src/main/java/com/hotmail/sami/config/WebInitializer.java
@@ -5,16 +5,7 @@ import javax.servlet.ServletException;
 
 import org.springframework.web.WebApplicationInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-import javax.servlet.ServletRegistration.Dynamic;
-
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+ 
 
 public class WebInitializer implements WebApplicationInitializer {
 
